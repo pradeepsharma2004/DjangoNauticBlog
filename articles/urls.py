349dd185678f7beeff53f442ac1ffea5a6cb2fd3
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-06-03 23:06:06 +0530

    urls.py added to articles app

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-urlpatters = [
-    url(r'^$', views.articles_list)
+app_name = 'articles'
 
+urlpatterns = [
+    url(r'^$', views.articles_list, name="list"),
+    url(r'^create/$', views.article_create, name="create"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail")
 ]
 
