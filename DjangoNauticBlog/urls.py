d5eabff16e28f75cfcdfe28bfe05e6f39fb73a04
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-06-07 17:53:11 +0530

    urls.py added to DjangoNauticBlog app

--- DjangoNauticBlog/urls.py
+++ DjangoNauticBlog/urls.py
@@ -15,11 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from . import settings
+from articles import views as article_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^about/$', views.about),
-    url(r'^$', views.index),
+    url(r'^$', article_views.articles_list, name='home'),
+
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
